c2d761f163f2f8c7f8b0b516796f2edda3aecb25
Anastasia Gracheva <gracheva.as@phystech.edu>
2021-01-31 01:16:01 +0300

    game draft(2)

--- game.py
+++ game.py
@@ -1,29 +1,37 @@
-from quantum_logic import Battleship
+import numpy as np
+
+from quantum_logic import Battleship, QuantumGame
 
 
 class GameField:
-    def __init__(self, draw_next_ship, color_cell, field_size=12, qbits=11, total_ships=10):
+    def __init__(self, draw_next_ship, color_cell, field_size=12, qbits=11, max_qbits_per_ship=3,
+                 ship_sizes=(4, 3, 3, 2, 2, 2, 1, 1, 1, 1)):
         self.field_size = field_size
         self.ships = []
-        self.current_size = None
+        self.nship_current = None
         self.current_ship = None
         self.qbits_total = qbits
         self.qbits_remaining = qbits
-        self.ships_total = total_ships
+        self.qbits_per_ship = max_qbits_per_ship
+        self.ship_sizes = ship_sizes
         self.draw_in_progress = False
         self.draw_next_ship = draw_next_ship
         self.color_cell = color_cell
+        self.qgame = None
 
     def next_ship_clicked(self):
         self.draw_in_progress = True
-        if self.current_ship is not None or self.current_size is not None:
+        if self.nship_current is None:
+            self.nship_current = 0
+        else:
             if len(self.current_ship) == 0:
                 return "No current ships placed"
-            self.ships.append(Battleship(shape=self.current_size, coordinates=self.current_ship))
+            self.ships.append(Battleship(shape=self.ship_sizes[self.nship_current], coordinates=self.current_ship))
             self.qbits_remaining -= int(np.ceil(np.log2(len(self.current_ship))))
-        if len(self.ships) >= self.ships_total:
-            self.current_ship = []
-            self.current_size = size
+        self.current_ship = []
+        self.nship_current += 1
+        if self.nship_current < len(self.ship_sizes):
+            self.draw_next_ship(size=self.ship_sizes[self.nship_current])
 
     # 0 - horizontal, 1 - vertical
     def cell_clicked(self, x, y, orientation=None):
@@ -35,7 +43,5 @@ class GameField:
                 self.draw_in_progress = False
                 return "No qbits left"
         else:
-            pass
-
-    def finished_drawing(self):
-        self.draw_in_progress = False
+            if self.qgame is None:
+                self.qgame = QuantumGame(self.ships)
