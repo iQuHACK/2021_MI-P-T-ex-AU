2621d0d8d5ba1f4fb8234f37c100c340cefe267c
Dmitrii Morozov <dmitrii.morozov@polytechnique.edu>
2021-01-31 14:40:09 +0300

    Update ships_test.py

--- ships_test.py
+++ ships_test.py
@@ -1,4 +1,5 @@
 from quantum_logic import *
+from transfer_data import *
 
 f = open("ships.txt", 'r')
 line = f.readline()
@@ -19,7 +20,17 @@ for line in f:
     print(ships[-1].coordinates, ships[-1].shape)
 f.close()
 game = QuantumGame(ships, int(shoots), int(size))
-result = game.qubit_sets_from_ships()
-print(lines)
-for i in result:
-    print(i.ship_ids, i.intersection_ids)
+result = send_ships(game.ships)
+result = result.decode()
+ships = get_ships(result)
+print(result)
+for i in ships:
+    print(i.coordinates, i.shape)
+
+shoots = [[1, 1, 2, 1], [2, 5, 4, 1], [4, 2, 1, 0], [5, 7, 9, 1]]
+print(shoots)
+test = send_shots_results(shoots).decode()
+print(test)
+print(get_shots_result(test))
+
+
