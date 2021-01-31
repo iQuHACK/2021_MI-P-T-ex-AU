23bc35297b3729367101e45c8d284b6eaa78ad3f
Dmitrii Morozov <dmitrii.morozov@polytechnique.edu>
2021-01-31 03:34:36 +0300

    first test

--- ships_test.py
+++ ships_test.py
@@ -3,8 +3,8 @@ from quantum_logic import *
 f = open("ships.txt", 'r')
 line = f.readline()
 shoots, size = line.split(' ')
-line = f.readline()
-sets = line.split(', ')
+lines = f.readline()
+sets = lines.split(', ')
 sets_from_ships = []
 for i in sets:
     sets_from_ships.append([int(j) for j in i.split(' ')])
@@ -17,5 +17,9 @@ for line in f:
         coords.append((int(params[3*i+1]), int(params[3*i+2]), int(params[3*i+3])))
     ships.append(Battleship(int(params[0]), coords))
 f.close()
-game = QuantumGame(ships, 5, size)
-print(game.qubit_sets_from_ships())
+game = QuantumGame(ships, int(shoots), int(size))
+result = game.qubit_sets_from_ships()
+print(lines)
+for i in result:
+    print(i.ship_ids, end=', ')
+
